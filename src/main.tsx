2aeb94b4f76d5ce2b7919aa15a3092f2eca101ab
TunkShif <tunkshif@foxmail.com>
2022-12-26 21:49:42 +0800

    update: example

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,29 @@
 import { createElement, render } from "./react"
 
-const element = (
-  <div id="foo">
-    <h1 className="heading">Hello, World!</h1>
-    <div className="section">
-      <p>proto react test</p>
-      <a href="#">link</a>
+const renderer = (value: string) => {
+  const element = (
+    <div id="foo">
+      <h1 className="heading">Hello, World!</h1>
+      <div className="section">
+        <p>proto react test</p>
+        <a href="#">link</a>
+      </div>
+      <div>
+        <button onClick={() => alert("hello")}>greet</button>
+      </div>
+      <div>
+        <input
+          onInput={(e: Event) => {
+            renderer((e.target as HTMLInputElement).value)
+          }}
+          value={value}
+        />
+        <p>Hello, {value}!</p>
+      </div>
     </div>
-    <div>
-      <button onClick={() => alert("hello")}>greet</button>
-    </div>
-  </div>
-)
+  )
 
-render(element, document.getElementById("app")!)
+  render(element, document.getElementById("app")!)
+}
+
+renderer("World")
